cd1cac2fa32bf772bfb97be06990298b5e41737f
john9101 <21130319@st.hcmuaf.edu.vn>
2025-04-20 15:05:10 +0700

    restore database setting

--- src/database/db_setting.py
+++ src/database/db_setting.py
@@ -10,7 +10,7 @@ def get_connection() -> MySQLConnection:
     # )
     return mysql.connector.connect(
         host='localhost',
-        user='root',
-        password='password',
+        user='admin',
+        password='admin',
         database='celeb_match',
     )
