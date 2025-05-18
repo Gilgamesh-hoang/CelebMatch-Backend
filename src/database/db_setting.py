fbb74617fbbf0559ce9f8899914f60c2420544d4
john9101 <21130319@st.hcmuaf.edu.vn>
2025-05-18 11:16:27 +0700

    restore db password

--- src/database/db_setting.py
+++ src/database/db_setting.py
@@ -5,7 +5,7 @@ def get_connection() -> MySQLConnection:
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='password',
+        password='',
         database='celeb_match',
     )
     # return mysql.connector.connect(
